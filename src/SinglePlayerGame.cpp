41ad8fa7d1d1bab4cfc2bd8f9ed1fc2676027f49
All23tor <158845088+All23tor@users.noreply.github.com>
2024-12-23 17:28:33 -0500

    Update and rename Game.cpp to SinglePlayerGame.cpp

---
+++ src/SinglePlayerGame.cpp
@@ -0,0 +1,28 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "Playfield.hpp"
+#include <stack>
+
+class Game {
+  const DrawingDetails drawingDetails;
+  Playfield playfield;
+  bool paused = false;
+  // Previous moves
+  std::stack<Playfield> undoMoveStack;
+  // Controller
+  const Controller controller;
+  const static Controller KeyboardControls;
+  const static Controller KeyboardControls2;
+
+private:
+  void update();
+  void DrawPauseMenu() const;
+  void draw() const;
+
+public:
+  Game();
+  void run();
+};
+
+#endif
