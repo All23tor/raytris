a9fef0f071e549437808bf2a09c4826f16bfcfea
All23tor <acc.dummyagta1@gmail.com>
2025-01-02 23:49:35 -0500

    Web version now works as it should, finally.

--- main-web.cpp
+++ main-web.cpp
@@ -1,21 +1,46 @@
 #include <emscripten/emscripten.h>
 #include "SinglePlayerGame.hpp"
+#include "Menu.hpp"
+#include <variant>
 
 namespace RaylibWeb {
   constexpr int windowWidth = 800, windowHeight = 450;
 
   namespace {
-    SinglePlayerGame singlePlayerGame{ {
-      DrawingDetails::HeightScaleFactor * windowHeight / (Playfield::VisibleHeight),
-      {windowWidth / 2.0f, windowHeight / 2.0f}
-    } };
+    std::variant<Menu, SinglePlayerGame> raytris = Menu();
+
+    auto handleWhereToGo = [](auto&& runnable) -> void {
+      using T = std::decay_t<decltype(runnable)>;
+      if constexpr (std::is_same_v<T, Menu>) {
+        switch (runnable.getSelectedOption()) {
+        case Menu::Option::Exit: return;
+        case Menu::Option::SinglePlayer: {
+          raytris.emplace<SinglePlayerGame>(DrawingDetails{
+            DrawingDetails::HeightScaleFactor * windowHeight / (Playfield::VisibleHeight),
+            { windowWidth / 2.0f, windowHeight / 2.0f }
+            });
+          return;
+        }
+        case Menu::Option::TwoPlayers: return;
+        }
+      } else if constexpr (std::is_same_v<T, SinglePlayerGame>) {
+        raytris.emplace<Menu>();
+      } else {
+        static_assert(false, "non-exhaustive visitor!");
+      }
+    };
   }
 
   void UpdateDrawFrame() {
-    singlePlayerGame.update();
+    std::visit(&Updateable::update, raytris);
+
+    if (!std::visit(&Runnable::shouldStopRunning, raytris)) {
+      std::visit(handleWhereToGo, raytris);
+    }
+
     BeginDrawing();
     ClearBackground(DrawingDetails::BackgroundColor);
-    singlePlayerGame.draw();
+    std::visit(&Drawable::draw, raytris);
     EndDrawing();
   }
 }
