63ae281641fb46d2df3d4e3c5932fbb6367d3a0e
All23tor <acc.dummyagta1@gmail.com>
2025-01-03 21:09:21 -0500

    main-web can now handle TwoPlayerGames

--- main-web.cpp
+++ main-web.cpp
@@ -1,25 +1,29 @@
 #include <emscripten/emscripten.h>
 #include "SinglePlayerGame.hpp"
+#include "TwoPlayerGame.hpp"
 #include "Menu.hpp"
 #include <variant>
 
 namespace RaylibWeb {
   namespace {
     Menu menu;
-    std::variant<Menu*, SinglePlayerGame> raytris = &menu;
+    std::variant<Menu*, SinglePlayerGame, TwoPlayerGame> raytris = &menu;
 
     auto handleWhereToGo = [](auto&& runnable) -> void {
       using T = std::decay_t<decltype(runnable)>;
       if constexpr (std::is_same_v<T, Menu*>) {
         switch (runnable->getSelectedOption()) {
         case Menu::Option::Exit: return;
-        case Menu::Option::SinglePlayer: {
+        case Menu::Option::SinglePlayer:
           raytris.emplace<SinglePlayerGame>();
-          return;
-        }
-        case Menu::Option::TwoPlayers: return;
+          break;
+        case Menu::Option::TwoPlayers:
+          raytris.emplace<TwoPlayerGame>();
+          break;
         }
       } else if constexpr (std::is_same_v<T, SinglePlayerGame>) {
+        raytris.emplace<Menu*>(&menu);
+      } else if constexpr (std::is_same_v<T, TwoPlayerGame>) {
         raytris.emplace<Menu*>(&menu);
       } else {
         static_assert(false, "non-exhaustive visitor!");
