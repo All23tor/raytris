a481f7219348f18a24bdf01b5cb7b89526f4fdaa
All23tor <acc.dummyagta1@gmail.com>
2025-01-24 15:25:10 -0500

    Fixed indentation

--- src/DrawingDetails.cpp
+++ src/DrawingDetails.cpp
@@ -2,8 +2,8 @@
 #include "Playfield.hpp"
 
 DrawingDetails::DrawingDetails(float _blockLength, Vector2 _position):
-    blockLength(_blockLength),
-    fontSize(blockLength * 2),
-    fontSizeBig(blockLength * 5),
-    fontSizeSmall(blockLength),
-    position{_position.x - blockLength * Playfield::Width / 2, _position.y - blockLength * Playfield::VisibleHeight / 2} {}
+  blockLength(_blockLength),
+  fontSize(blockLength * 2),
+  fontSizeBig(blockLength * 5),
+  fontSizeSmall(blockLength),
+  position{_position.x - blockLength * Playfield::Width / 2, _position.y - blockLength * Playfield::VisibleHeight / 2} {}
