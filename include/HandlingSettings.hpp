cd4efa989d1fdbf20fb718e497801d21a8e904a8
All23tor <acc.dummyagta1@gmail.com>
2025-08-19 16:03:35 -0500

    Moved serialization of HandlingSettings to SettingsMenu.cpp

--- include/HandlingSettings.hpp
+++ include/HandlingSettings.hpp
@@ -1,33 +1,12 @@
 #ifndef HANDLING_SETTINGS_HPP
 #define HANDLING_SETTINGS_HPP
 
-#include <iostream>
-
 struct HandlingSettings {
   int gravityFrames;
   int softDropFrames;
   int maxLockDelayFrames;
   int maxLockDelayResets;
   int das;
-
-  friend std::ostream& operator<<(std::ostream& o,
-                                  const HandlingSettings& settings) {
-    o << settings.gravityFrames << ' ';
-    o << settings.softDropFrames << ' ';
-    o << settings.maxLockDelayFrames << ' ';
-    o << settings.maxLockDelayResets << ' ';
-    o << settings.das << '\n';
-    return o;
-  }
-
-  friend std::istream& operator>>(std::istream& i, HandlingSettings& settings) {
-    i >> settings.gravityFrames;
-    i >> settings.softDropFrames;
-    i >> settings.maxLockDelayFrames;
-    i >> settings.maxLockDelayResets;
-    i >> settings.das;
-    return i;
-  }
 };
 
 #endif
--- src/SettingsMenu.cpp
+++ src/SettingsMenu.cpp
@@ -5,6 +5,26 @@
 #include <fstream>
 #include <utility>
 
+namespace {
+std::ostream& operator<<(std::ostream& o, const HandlingSettings& settings) {
+  o << settings.gravityFrames << ' ';
+  o << settings.softDropFrames << ' ';
+  o << settings.maxLockDelayFrames << ' ';
+  o << settings.maxLockDelayResets << ' ';
+  o << settings.das << '\n';
+  return o;
+}
+
+std::istream& operator>>(std::istream& i, HandlingSettings& settings) {
+  i >> settings.gravityFrames;
+  i >> settings.softDropFrames;
+  i >> settings.maxLockDelayFrames;
+  i >> settings.maxLockDelayResets;
+  i >> settings.das;
+  return i;
+}
+} // namespace
+
 Resolution SettingsMenu::resolution = [] {
   std::ifstream in("settings.raytris");
   if (!in.good())
