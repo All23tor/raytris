6874103d403ccc53e0eff68ba173c89dfe3da232
All23tor <acc.dummyagta1@gmail.com>
2025-08-18 23:23:01 -0500

    Removed unnecesary file src/HandlingSettings.cpp

--- include/HandlingSettings.hpp
+++ include/HandlingSettings.hpp
@@ -10,8 +10,24 @@ struct HandlingSettings {
   int maxLockDelayResets;
   int das;
 
-  friend std::ostream& operator<<(std::ostream&, const HandlingSettings&);
-  friend std::istream& operator>>(std::istream&, HandlingSettings&);
+  friend std::ostream& operator<<(std::ostream& o,
+                                  const HandlingSettings& settings) {
+    o << settings.gravityFrames << ' ';
+    o << settings.softDropFrames << ' ';
+    o << settings.maxLockDelayFrames << ' ';
+    o << settings.maxLockDelayResets << ' ';
+    o << settings.das << '\n';
+    return o;
+  }
+
+  friend std::istream& operator>>(std::istream& i, HandlingSettings& settings) {
+    i >> settings.gravityFrames;
+    i >> settings.softDropFrames;
+    i >> settings.maxLockDelayFrames;
+    i >> settings.maxLockDelayResets;
+    i >> settings.das;
+    return i;
+  }
 };
 
-#endif 
+#endif
